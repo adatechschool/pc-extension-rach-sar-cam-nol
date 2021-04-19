14d78f4a86e3827c21cbe3ca7b0523a879609215
Sabevi <sarabev6@gmail.com>
2021-04-19 15:38:38 +0200

    Latest version

--- src/Extension/replace.js
+++ src/Extension/replace.js
@@ -1,68 +1,46 @@
-function changeBackground() {
-  var pics = [
-    "img/landscape_1.jpg",
-    "img/landscape_2.jpg",
-    "img/landscape_3.jpg",
-    "img/landscape_4.jpg",
-    "img/landscape_5.jpg",
-    "img/landscape_6.jpg",
-    "img/landscape_7.jpg",
-    "img/landscape_8.jpg",
-    "img/landscape_9.jpg",
-    "img/landscape_10.jpg",
-    "img/landscape_11.jpg",
-    "img/landscape_12.jpg",
-    "img/landscape_13.jpg",
-    "img/landscape_14.jpg",
-    "img/landscape_15.jpg",
-    "img/landscape_16.jpg",
-    "img/landscape_17.jpg",
-    "img/landscape_18.jpg",
-    "img/landscape_19.jpg",
-    "img/landscape_20.jpg",
-  ];
-  var background = document.getElementById("backgroundImage");
+var pics = [
+  "img/landscape_2.jpg",
+  "img/landscape_3.jpg",
+  "img/landscape_4.jpg",
+  "img/landscape_5.jpg",
+  "img/landscape_6.jpg",
+  "img/landscape_7.jpg",
+  "img/landscape_8.jpg",
+];
 
-  var counter = 1;
-  document.addEventListener("click", function () {
-    if (counter === pics.length) {
+var quotes = [
+  "Yesterday I was clever, so I wanted to change the world. Today I am wise, so I am changing myself.",
+  "Raise your words, not voice. It is rain that grows flowers, not thunder.",
+  "It’s a good day to have a good day.",
+  "Feed your mind with positive thoughts, and attract great things in your life.",
+  "Be such a good soul that people crave your vibes.",
+  "You can’t stop the waves, but you can learn to surf.",
+  "Just enjoy where you are now.",
+];
+
+var background = document.getElementById("backgroundImage");
+var quoteToChange = document.getElementById("quoteToChange");
+
+var counter = 0;
+
+function updateContent(){
+  if (counter === pics.length) {
       counter = 0;
-    }
-    background.style.backgroundImage = `url('${pics[counter]}')`;
-    counter += 1;
-  });
+  }
+  background.style.backgroundImage = `url('${pics[counter]}')`;
+  quoteToChange.innerHTML = quotes[counter];
+  counter += 1;
+  replaceContent();
+  quoteAppear();
 }
 
-changeBackground();
-
-function changeQuote() {
-  var counterQuotes = 0;
-  var quotes = [
-    "Yesterday I was clever, so I wanted to change the world.Today I am wise, so I am changing myself.",
-    "Raise your words, not voice. It is rain that grows flowers, not thunder.",
-    "It’s a good day to have a good day.",
-    "Feed your mind with positive thoughts, and attract great things in your life.",
-    "Be such a good soul that people crave your vibes.",
-    "You can’t stop the waves, but you can learn to surf.",
-    "Just enjoy where you are now.",
-  ];
-  var quoteToChange = document.getElementById("quoteToChange");
-
-  document.addEventListener("click", function () {
-    if (counterQuotes === quotes.length) {
-      counterQuotes = 0;
-    }
-    quoteToChange.innerHTML = quotes[counterQuotes];
-    counterQuotes += 1;
-    quoteAppear();
-  });
+function replaceContent() {
+  setInterval(updateContent, 5000);
 }
-
-changeQuote();
 
 function quoteAppear() {
   var quote = document.getElementById("quoteToChange");
   quoteToChange.animate([{ opacity: 0 }, { opacity: 1 }], { duration: 1500 });
 }
 
-quoteAppear();
+updateContent(); 
